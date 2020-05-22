9be3e24b068479c87bc83f366fac4a85bc23c223
11kyle <11kyle.johnson@gmail.com>
2020-05-22 16:14:57 -0600

    Updates

---
+++ precache-manifest.c99a64d17d93f7e75f67b20e84889a68.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b761590cbfc4bb691d9b7eac2430890e",
+    "url": "/mern-client/index.html"
+  },
+  {
+    "revision": "7396315258cf651b381f",
+    "url": "/mern-client/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a12da507dc22a3419da7",
+    "url": "/mern-client/static/js/2.1a13da5f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mern-client/static/js/2.1a13da5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7396315258cf651b381f",
+    "url": "/mern-client/static/js/main.c9b02412.chunk.js"
+  },
+  {
+    "revision": "be98632a039c4d50927c",
+    "url": "/mern-client/static/js/runtime-main.e2c506e6.js"
+  },
+  {
+    "revision": "9152be177af58cd0aa28a6e0b33b7948",
+    "url": "/mern-client/static/media/man.9152be17.png"
+  },
+  {
+    "revision": "9d571721d35ff6776a51105906b8b943",
+    "url": "/mern-client/static/media/website_for_portfolio_01.9d571721.png"
+  },
+  {
+    "revision": "6d82890752e8ba0a87bfd4a85dc0cf0f",
+    "url": "/mern-client/static/media/website_for_portfolio_02.6d828907.png"
+  }
+]);
