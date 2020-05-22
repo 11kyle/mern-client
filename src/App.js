16666390a9aaa929b4d0d45a14cd911394bfbf28
11kyle <11kyle.johnson@gmail.com>
2020-05-22 07:25:50 -0600

    Installed axios and added state to the <App>.

--- src/App.js
+++ src/App.js
@@ -11,25 +11,56 @@ import Blog from './Components/Routes/Blog';
 import About from './Components/Routes/About';
 import { MuiThemeProvider, createMuiTheme, CssBaseline } from '@material-ui/core';
 
+const axios = require('axios');
+
 const theme = createMuiTheme({
   typography: { h2: { fontFamily: 'Quicksand, sans-serif'}}
 })
 
 export default class App extends Component {
+
+  constructor(props) {
+    super();
+    this.state = {
+      blogposts: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.getBlogposts();
+  }
+
+  getBlogposts = () => {
+    axios.get('https://kj-portfolio.herokuapp.com/api/blogpost')
+      .then(response => {
+        this.setState({
+          blogposts: response.data,
+          loading: false,
+        });
+      })
+      .catch(error => {
+        console.log(`Error fetching and parsing data`, error);
+      });
+  }
+
   render() {
     return(
       <Router>
         <MuiThemeProvider theme={theme}>
           <CssBaseline>
-            
             <Header />
             <Switch>
               <Route path='/portfolio'>
                 <Portfolio />
               </Route>
               <Route path='/blog'>
-                <Blog />
-              </Route>
+                {
+                  (this.state.loading)
+                    ?<p>Loading...</p>
+                    : <Blog blogposts={this.state.blogposts} />
+                }
+                  </Route>
               <Route path='/'>
                 <About />
               </Route>
--- src/Components/Routes/Blog.js
+++ src/Components/Routes/Blog.js
@@ -6,7 +6,30 @@ import {
     Box 
 } from '@material-ui/core'; 
 
-export default function Blog() {
+export default function Blog(props) {
+
+    const { blogposts } = props;
+    const listItems = blogposts.map((blogpost) => 
+        <Grid item xs={12} key={blogpost.id}>
+            <Paper elevation={2} style={{maxWidth: "600px" }}>
+                <Box p={2} mt={2}>
+                    <Typography variant="h6">
+                        {blogpost.title}
+                    </Typography>
+                    <Typography variant="subtitle2">
+                        Thursday, Sep 21, 2017 8:37 PM
+                    </Typography>
+                    <Typography>
+                        Author: {blogpost.author}
+                    </Typography>
+                    <Typography>
+                        {blogpost.body}
+                    </Typography>
+                </Box>
+            </Paper>
+        </Grid>
+    );
+
     return(
         <Grid
             container
@@ -15,41 +38,7 @@ export default function Blog() {
             spacing={0}
         >
             <Grid item xs={12}>
-                <Paper elevation={2} style={{maxWidth: "600px" }}>
-                    <Box p={2} mt={2}>
-                        <Typography variant="h6">
-                            Tutorial: How to setup and connect mLab.
-                        </Typography>
-                        <Typography variant="subtitle2">
-                            Thursday, Sep 21, 2017 8:37 PM
-                        </Typography>
-                        <Typography>
-                            Author: Kyle Johnson
-                        </Typography>
-                        <Typography>
-                            Stay tuned for a step by step guide on how setup and connect mLab.
-                        </Typography>
-                    </Box>
-                </Paper>
-            </Grid>
-
-            <Grid item xs={12}>
-                <Paper elevation={2} style={ {maxWidth: "600px" }}>
-                    <Box p={2} mt={2}>
-                        <Typography variant="h6">
-                            Welcome!
-                        </Typography>
-                        <Typography variant="subtitle2">
-                            Monday, Aug 7, 2017 5:16 PM
-                        </Typography>
-                        <Typography>
-                            Author: Kyle Johnson
-                        </Typography>
-                        <Typography>
-                        Welcome to my blog. Built with the MEAN stack, this is my way of showing off my skills. What is the MEAN stack? The MEAN stack is a complete JavaScript stack using MongoDB, Express.js, Angular.js and Node.js. MongoDB is a NoSQL database. Express.js and Node.js are back-end frameworks. Angular.js is the front-end framework. Put these together and you get this great blogging platform! Get it for yourself here.
-                        </Typography>
-                    </Box>
-                </Paper>
+                {listItems}
             </Grid>
         </Grid>
     );
